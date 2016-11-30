79093f6c7053601cc2cbb0e09ed52fe526337008
Josh Wilson <joshpwilson55@gmail.com>
2016-11-30 13:42:02 +1300

    Revert "Revert "work in progress""
    This reverts commit ddd73cf9fbcddc6be84b42fbe2a510475316a9e5.

--- minerals/urls.py
+++ minerals/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'(?P<name>\d+)/$', views.detail, name='detail'),
+    url(r'^$', views.index, name='list'),
+    url(r'^(?P<name>\d+)/$', views.mineral_detail, name='detail'),
 ]
 
